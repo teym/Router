32382a9c084e56426a604089a95020889c954afe
Mike <ccteym@gmail.com>
2017-10-09 10:23:31 +0800

    router

---
+++ Router/PathToReg.swift
@@ -0,0 +1,449 @@
+//
+//  PathToReg.swift
+//  Router
+//
+//  Created by 王航 on 2017/9/29.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import Foundation
+import SwiftyRe
+
+extension String {
+    func slice(_ start:Int, end:Int? = nil) -> String {
+        return Re.slice(self, start: start, end: end)
+    }
+    func at(_ index:Int) -> String {
+        return Re.slice(self, start: index, end: index+1)
+    }
+    var length:Int {
+        return (self as NSString).length
+    }
+}
+extension Array {
+    public func appendWith<S>(contentsOf newElements: S) -> Array where S : Sequence, Element == S.Element {
+        var arr = self
+        arr.append(contentsOf: newElements)
+        return arr
+    }
+}
+
+enum NameToken {
+    case Name(String)
+    case Index(Int)
+}
+struct TokenInfo {
+    let name:NameToken
+    let prefix:String
+    let delimiter:String
+    let optional:Bool
+    let i_repeat:Bool
+    let partial:Bool
+    let pattern:String
+    init(name:NameToken, prefix:String,delimiter:String,optional:Bool,i_repeat:Bool,partial:Bool,pattern:String) {
+        self.name = name
+        self.prefix = prefix
+        self.delimiter = delimiter
+        self.optional = optional
+        self.i_repeat = i_repeat
+        self.partial = partial
+        self.pattern = pattern
+    }
+    init(name:String, prefix:String,delimiter:String,optional:Bool,i_repeat:Bool,partial:Bool,pattern:String) {
+        self.init(name: .Name(name),
+                  prefix: prefix,
+                  delimiter: delimiter,
+                  optional: optional,
+                  i_repeat: i_repeat,
+                  partial: partial,
+                  pattern: pattern)
+    }
+    init(name:Int, prefix:String,delimiter:String,optional:Bool,i_repeat:Bool,partial:Bool,pattern:String) {
+        self.init(name: .Index(name),
+                  prefix: prefix,
+                  delimiter: delimiter,
+                  optional: optional,
+                  i_repeat: i_repeat,
+                  partial: partial,
+                  pattern: pattern)
+    }
+}
+enum ParseToken{
+    case Path(String)
+    case Token(TokenInfo)
+}
+
+func pathRegexp() ->Re {
+    return Re([
+    // Match escaped characters that would otherwise appear in future matches.
+    // This allows the user to escape special characters that won't transform.
+    "(\\\\.)",
+    // Match Express-style parameters and un-named parameters with a prefix
+    // and optional suffixes. Matches appear as:
+    //
+    // "/:test(\\d+)?" => ["/", "test", "\d+", undefined, "?"]
+    // "/route(\\d+)"  => [undefined, undefined, undefined, "\d+", undefined]
+    "(?:\\:(\\w+)(?:\\(((?:\\\\.|[^\\\\()])+)\\))?|\\(((?:\\\\.|[^\\\\()])+)\\))([+*?])?"
+    ].joined(separator: "|"),"g")
+    
+}
+
+/**
+ * Parse a string for the raw tokens.
+ *
+ * @param  {string}  str
+ * @param  {Object=} options
+ * @return {!Array}
+ */
+func parse(str:String, options:[String:AnyObject]) -> [ParseToken] {
+    var tokens = [ParseToken]()
+    var key = 0
+    var index = 0
+    var path = ""
+    let defaultDelimiter = (options["delimiter"] as? String) ?? "/"
+    let delimiters = (options["delimiters"] as? String) ?? "./"
+    var pathEscaped = false
+    
+    let PATH_REGEXP = pathRegexp()
+    while let ret = PATH_REGEXP.exec(str) {
+        let res = ret.values
+        let offset = ret.index
+        let m = res[0]
+        let escaped = res[1]
+        path += str.slice(index, end: offset)
+        index = offset + m.length
+        
+        // Ignore already escaped sequences.
+        if !escaped.isEmpty {
+            path.append(escaped.at(1))
+            pathEscaped = true
+            continue
+        }
+    
+        var prev = ""
+        let next = str.at(index)
+        let name = res[2]
+        let capture = res[3]
+        let group = res[4]
+        let modifier = res[5]
+    
+        if (!pathEscaped && path.length > 0) {
+            let k = path.length - 1
+            if delimiters.contains(path.at(k)) {
+                prev = path.at(k)
+                path = path.slice(0, end: k)
+          }
+        }
+    
+        // Push the current path onto the tokens.
+        if (path.length > 0) {
+            tokens.append(.Path(path))
+            path = ""
+            pathEscaped = false
+        }
+    
+        let partial = !prev.isEmpty && !next.isEmpty && next != prev
+        let i_repeat = modifier == "+" || modifier == "*"
+        let optional = modifier == "?" || modifier == "*"
+        let delimiter = !prev.isEmpty ? prev : defaultDelimiter
+        let pattern = capture.isEmpty && group.isEmpty ?
+            "[^" + escapeString(str: delimiter) + "]+?" :
+            escapeGroup(group: capture.isEmpty ? group : capture)
+
+        if !name.isEmpty {
+            tokens.append(.Token(TokenInfo(name: name,
+                                           prefix: prev,
+                                           delimiter: delimiter,
+                                           optional: optional,
+                                           i_repeat: i_repeat,
+                                           partial: partial,
+                                           pattern: pattern)))
+        }else {
+            tokens.append(.Token(TokenInfo(name: key,
+                                           prefix: prev,
+                                           delimiter: delimiter,
+                                           optional: optional,
+                                           i_repeat: i_repeat,
+                                           partial: partial,
+                                           pattern: pattern)))
+            key += 1
+        }
+      }
+    
+      // Push any remaining characters.
+      if (!path.isEmpty || index < str.length) {
+        let p = path + str.slice(index)
+        tokens.append(.Path(p))
+      }
+
+      return tokens
+}
+//
+///**
+// * Compile a string to a template function for the path.
+// *
+// * @param  {string}             str
+// * @param  {Object=}            options
+// * @return {!function(Object=, Object=)}
+// */
+//function compile (str, options) {
+//  return tokensToFunction(parse(str, options))
+//}
+//
+///**
+// * Expose a method for transforming tokens into the path function.
+// */
+//function tokensToFunction (tokens) {
+//  // Compile all the tokens into regexps.
+//  var matches = new Array(tokens.length)
+//
+//  // Compile all the patterns before compilation.
+//  for (var i = 0; i < tokens.length; i++) {
+//    if (typeof tokens[i] === 'object') {
+//      matches[i] = new RegExp('^(?:' + tokens[i].pattern + ')$')
+//    }
+//  }
+//
+//  return function (data, options) {
+//    var path = ''
+//    var encode = (options && options.encode) || encodeURIComponent
+//
+//    for (var i = 0; i < tokens.length; i++) {
+//      var token = tokens[i]
+//
+//      if (typeof token === 'string') {
+//        path += token
+//        continue
+//      }
+//
+//      var value = data ? data[token.name] : undefined
+//      var segment
+//
+//      if (Array.isArray(value)) {
+//        if (!token.repeat) {
+//          throw new TypeError('Expected "' + token.name + '" to not repeat, but got array')
+//        }
+//
+//        if (value.length === 0) {
+//          if (token.optional) continue
+//
+//          throw new TypeError('Expected "' + token.name + '" to not be empty')
+//        }
+//
+//        for (var j = 0; j < value.length; j++) {
+//          segment = encode(value[j])
+//
+//          if (!matches[i].test(segment)) {
+//            throw new TypeError('Expected all "' + token.name + '" to match "' + token.pattern + '"')
+//          }
+//
+//          path += (j === 0 ? token.prefix : token.delimiter) + segment
+//        }
+//
+//        continue
+//      }
+//
+//      if (typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean') {
+//        segment = encode(String(value))
+//
+//        if (!matches[i].test(segment)) {
+//          throw new TypeError('Expected "' + token.name + '" to match "' + token.pattern + '", but got "' + segment + '"')
+//        }
+//
+//        path += token.prefix + segment
+//        continue
+//      }
+//
+//      if (token.optional) {
+//        // Prepend partial segment prefixes.
+//        if (token.partial) path += token.prefix
+//
+//        continue
+//      }
+//
+//      throw new TypeError('Expected "' + token.name + '" to be ' + (token.repeat ? 'an array' : 'a string'))
+//    }
+//
+//    return path
+//  }
+//}
+
+/**
+ * Escape a regular expression string.
+ *
+ * @param  {string} str
+ * @return {string}
+ */
+func escapeString (str:String) -> String {
+    return Re("([\\.\\+\\*\\?\\=\\^\\!\\:\\$\\{\\}\\(\\)\\[\\]\\|\\/\\\\])", "g").replace(str, {"\\" + $0.values[1]})
+//  return str.replace(/([.+*?=^!:${}()[\]|/\\])/g, '\\$1')
+}
+
+/**
+ * Escape the capturing group by escaping special characters and meaning.
+ *
+ * @param  {string} group
+ * @return {string}
+ */
+func escapeGroup (group:String) -> String {
+    return Re("([\\=\\!\\:\\$\\/\\(\\)])","g").replace(group, {"\\" + $0.values[1]})
+//  return group.replace(/([=!:$/()])/g, '\\$1')
+}
+
+/**
+ * Get the flags for a regexp from the options.
+ *
+ * @param  {Object} options
+ * @return {string}
+ */
+func flags(options:[String:AnyObject]) -> String {
+    let sensitive = (options["sensitive"] as? Bool) ?? false
+    return sensitive ? "" : "i"
+}
+
+///**
+// * Pull out keys from a regexp.
+// *
+// * @param  {!RegExp} path
+// * @param  {Array=}  keys
+// * @return {!RegExp}
+// */
+//function regexpToRegexp (path, keys) {
+//  if (!keys) return path
+//
+//  // Use a negative lookahead to match only capturing groups.
+//  var groups = path.source.match(/\((?!\?)/g)
+//
+//  if (groups) {
+//    for (var i = 0; i < groups.length; i++) {
+//      keys.push({
+//        name: i,
+//        prefix: null,
+//        delimiter: null,
+//        optional: false,
+//        repeat: false,
+//        partial: false,
+//        pattern: null
+//      })
+//    }
+//  }
+//
+//  return path
+//}
+//
+///**
+// * Transform an array into a regexp.
+// *
+// * @param  {!Array}  path
+// * @param  {Array=}  keys
+// * @param  {Object=} options
+// * @return {!RegExp}
+// */
+//function arrayToRegexp (path, keys, options) {
+//  var parts = []
+//
+//  for (var i = 0; i < path.length; i++) {
+//    parts.push(pathToRegexp(path[i], keys, options).source)
+//  }
+//
+//  return new RegExp('(?:' + parts.join('|') + ')', flags(options))
+//}
+
+/**
+ * Create a path regexp from string input.
+ *
+ * @param  {string}  path
+ * @param  {Array=}  keys
+ * @param  {Object=} options
+ * @return {!RegExp}
+ */
+func stringToRegexp (path:String, options:[String:AnyObject] = [:]) -> (Re,[TokenInfo]) {
+    return tokensToRegExp(tokens:parse(str:path, options:options), options:options)
+}
+
+/**
+ * Expose a function for taking tokens and returning a RegExp.
+ *
+ * @param  {!Array}  tokens
+ * @param  {Array=}  keys
+ * @param  {Object=} options
+ * @return {!RegExp}
+ */
+func tokensToRegExp (tokens:[ParseToken], options:[String:AnyObject]) -> (Re,[TokenInfo]) {
+//  options = options || {}
+    
+    let strict = (options["strict"] as? Bool) ?? false
+    let end = (options["end"] as? Bool) ?? true
+    let delimiter = escapeString(str: (options["delimiter"] as? String) ?? "/")
+    let endsWith = [String]().appendWith(contentsOf: ([options["endsWith"]] as? [String]) ?? []).map({escapeString(str: $0)}).appendWith(contentsOf:["$"]).joined(separator: "|")
+    var route = ""
+    var keys = [TokenInfo]()
+
+  // Iterate over the tokens and create our regexp string.
+    for i in 0..<tokens.count {
+        let token = tokens[i]
+        switch token {
+        case .Path(let path):
+            route += escapeString(str: path)
+        case .Token(let token):
+            let prefix = escapeString(str: token.prefix)
+            var capture = "(?:" + token.pattern + ")"
+            
+            keys.append(token)
+            
+            if (token.i_repeat) {
+                capture += "(?:" + prefix + capture + ")*"
+            }
+            
+            if (token.optional) {
+                if (!token.partial) {
+                    capture = "(?:" + prefix + "(" + capture + "))?"
+                } else {
+                    capture = prefix + "(" + capture + ")?"
+                }
+            } else {
+                capture = prefix + "(" + capture + ")"
+            }
+            
+            route += capture
+        }
+    }
+    
+    if (end) {
+        if (!strict){
+            route += "(?:" + delimiter + ")?"
+        }
+        route += endsWith == "$" ? "$" : "(?=" + endsWith + ")"
+    } else {
+        if (!strict) {
+            route += "(?:" + delimiter + "(?=" + endsWith + "))?"
+        }
+        route += "(?=" + delimiter + "|" + endsWith + ")"
+    }
+
+    return (Re("^" + route, flags(options: options)) , keys)
+}
+//
+///**
+// * Normalize the given path string, returning a regular expression.
+// *
+// * An empty array can be passed in for the keys, which will hold the
+// * placeholder key descriptions. For example, using `/user/:id`, `keys` will
+// * contain `[{ name: 'id', delimiter: '/', optional: false, repeat: false }]`.
+// *
+// * @param  {(string|RegExp|Array)} path
+// * @param  {Array=}                keys
+// * @param  {Object=}               options
+// * @return {!RegExp}
+// */
+//function pathToRegexp (path, keys, options) {
+//  if (path instanceof RegExp) {
+//    return regexpToRegexp(path, keys)
+//  }
+//
+//  if (Array.isArray(path)) {
+//    return arrayToRegexp(/** @type {!Array} */ (path), keys, options)
+//  }
+//
+//  return stringToRegexp(/** @type {string} */ (path), keys, options)
+//}
--- Router/Router.swift
+++ Router/Router.swift
@@ -7,3 +7,189 @@
 //
 
 import Foundation
+import UIKit
+import Module
+
+public protocol Router {
+    var root:UINavigationController?{get}
+    func push(path:String)
+    func replace(path:String)
+    func pop()
+    
+    func addRouter(path:String,comptent:@escaping (String,[String:String])->UIViewController?)
+    func addDefaultRouter(comptent:@escaping (String,[String:String])->UIViewController?)
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void) -> Router
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void, root:UINavigationController?) -> Router
+}
+
+class MRouter:NSObject, Router, Module {
+    static func interfaces() -> [AnyObject] {
+        return [Router.self as AnyObject]
+    }
+    static func loadOnStart() -> Bool {
+        return false
+    }
+    let router:PathRouter
+    required init(inject: ModuleInject) {
+        let nav = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
+        self.router = PathRouter(parent: nil, root: nav)
+        super.init()
+    }
+
+    var root: UINavigationController?{
+        return self.router.root
+    }
+    func push(path:String){
+        self.router.push(path: path)
+    }
+    func replace(path:String){
+        self.router.replace(path: path)
+    }
+    func pop(){
+        self.router.pop()
+    }
+    func addRouter(path:String,comptent:@escaping (String,[String:String])->UIViewController?){
+        self.router.addRouter(path: path, comptent: comptent)
+    }
+    func addDefaultRouter(comptent:@escaping (String,[String:String])->UIViewController?){
+        self.router.addDefaultRouter(comptent: comptent)
+    }
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void) -> Router {
+        return self.router.addSubRouter(path: path, comptent: comptent)
+    }
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void, root:UINavigationController?) -> Router{
+        return self.router.addSubRouter(path: path, comptent: comptent, root: root)
+    }
+}
+
+class PathRouter:NSObject,Router {
+    struct Comptent {
+        typealias MatchParser = (String)->[String:String]?
+        typealias Comptenter = (String,[String:String]) -> UIViewController?
+        let parser: MatchParser
+        let comptenter: Comptenter
+        let isDefault:Bool
+        init(parser: @escaping MatchParser,comptenter: @escaping Comptenter,isDefault:Bool = false) {
+            self.parser = parser
+            self.comptenter = comptenter
+            self.isDefault = isDefault
+        }
+    }
+    
+    let root:UINavigationController?
+    weak var parent:PathRouter?
+    var comptents = [Comptent]()
+    
+    init(parent:PathRouter?, root:UINavigationController? = nil) {
+        self.parent = parent
+        self.root = root
+        super.init()
+    }
+    
+    func push(path:String){
+        if let nav = self.root {
+            if let controller = self.comptent(url: path) {
+                nav.pushViewController(controller, animated: true)
+            }
+        }else {
+            self.parent?.push(path: path)
+        }
+    }
+    func replace(path:String){
+        if let nav = self.root {
+            if let controller = self.comptent(url: path) {
+                var list = nav.viewControllers
+                _ = list.popLast()
+                list.append(controller)
+                nav.setViewControllers(list, animated: false)
+            }
+        }else {
+            self.parent?.replace(path: path)
+        }
+    }
+    func pop(){
+        if let nav = self.root {
+            nav.popViewController(animated: true)
+        }else {
+            self.parent?.pop()
+        }
+    }
+    func addComptent(comptent:Comptent){
+        guard comptent.isDefault else {
+            return comptents.append(comptent)
+        }
+        if let index = comptents.index(where: {$0.isDefault}){
+            comptents.insert(comptent, at: index)
+        }else {
+            comptents.append(comptent)
+        }
+    }
+    func addRouter(path:String,comptent:@escaping (String,[String:String])->UIViewController?){
+        let (re, keys) = stringToRegexp(path: path)
+        let names = ["matched"].appendWith(contentsOf: keys.map({ (t) -> String in
+            switch t.name {
+            case .Index(let i):
+                return "\(i)"
+            case .Name(let s):
+                return s
+            }
+        }))
+        let parser = { (url:String) -> [String:String]? in
+            if let ret = re.match(url) {
+                assert(names.count == ret.values.count)
+                return Dictionary<String,String>(zip(names, ret.values),uniquingKeysWith: { (first, _) in first })
+            }
+            return nil
+        }
+        let comptenter = { (url:String,parameters:[String:String]) -> UIViewController? in
+            return comptent(url, parameters)
+        }
+        addComptent(comptent: Comptent(parser: parser,comptenter: comptenter))
+    }
+    func addDefaultRouter(comptent:@escaping (String,[String:String])->UIViewController?){
+        let parser = { (url:String) -> [String:String]? in
+            return ["matched":url]
+        }
+        let comptenter = { (url:String,parameters:[String:String]) -> UIViewController? in
+            return comptent(url, parameters)
+        }
+        addComptent(comptent: Comptent(parser: parser, comptenter: comptenter, isDefault:true))
+    }
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void) -> Router {
+        return self.addSubRouter(path: path, comptent: comptent, root: nil)
+    }
+    func addSubRouter(path:String, comptent:@escaping (String,[String:String])->Void, root:UINavigationController?) -> Router{
+        let router = PathRouter(parent: self, root: root)
+        let (re, keys) = stringToRegexp(path: path)
+        let names = ["matched"].appendWith(contentsOf: keys.map({ (t) -> String in
+            switch t.name {
+            case .Index(let i):
+                return "\(i)"
+            case .Name(let s):
+                return s
+            }
+        }))
+        let parser = { (url:String) -> [String:String]? in
+            if let ret = re.match(url) {
+                assert(names.count == ret.values.count)
+                return Dictionary<String,String>(zip(names, ret.values),uniquingKeysWith: { (first, _) in first })
+            }
+            return nil
+        }
+        let comptenter = { (url:String,parameters:[String:String]) -> UIViewController? in
+            comptent(url,parameters)
+            return router.comptent(url: url)
+        }
+        addComptent(comptent: Comptent(parser: parser,comptenter: comptenter))
+        return router
+    }
+    
+    func comptent(url:String)->UIViewController? {
+        for comptent in self.comptents {
+            if let parameters = comptent.parser(url) {
+                return comptent.comptenter(url, parameters)
+            }
+        }
+        return nil
+    }
+}
